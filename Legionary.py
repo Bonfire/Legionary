59989e7929ecb767011356fe537e152fe826b3d1
Baran Barut <bonfire.hf@gmail.com>
2018-11-09 20:31:59 -0500

    Reworked the kick and remove commands. New members added.

--- Legionary.py
+++ Legionary.py
@@ -6,7 +6,6 @@ from discord.ext import commands
 
 # Other Libraries
 import datetime
-import os
 import re
 
 bot = commands.Bot(command_prefix="!")
@@ -17,7 +16,7 @@ botToken = tokenFile.readline()
 tokenFile.close()
 
 # News Channel ID
-newsID = '508111231937413151'
+newsID = '463472622127284234'
 
 # Talk Channel ID
 talkID = '463477926961348643'
@@ -29,177 +28,167 @@ membersMessages = ['', '', '', '', '', '', '']
 
 @bot.event
 async def on_ready():
-    print('Logged in as ' + bot.user.name + ' (ID:' + bot.user.id + ') | Connected to ' + str(
-        len(bot.servers)) + ' servers | Connected to ' + str(len(set(bot.get_all_members()))) + ' users')
-    print('Current Discord.py Version: {} | Current Python Version: {}'.format(discord.__version__,
-                                                                               platform.python_version()))
+	print('Logged in as ' + bot.user.name + ' (ID:' + bot.user.id + ') | Connected to ' + str(
+		len(bot.servers)) + ' servers | Connected to ' + str(len(set(bot.get_all_members()))) + ' users')
+	print('Current Discord.py Version: {} | Current Python Version: {}'.format(discord.__version__,
+																			   platform.python_version()))
 
 
 @bot.event
 async def on_message(ctx):
-    # Only allow the staff to use these commands
-    authorRoles = [role.name for role in ctx.author.roles]
-    if 'Captain' in authorRoles or 'General' in authorRoles or 'Owner' in authorRoles:
-        # Process everything else as a command
-        await bot.process_commands(ctx)
+	# Only allow the staff to use these commands
+	authorRoles = [role.name for role in ctx.author.roles]
+	if 'Captain' in authorRoles or 'General' in authorRoles or 'Owner' in authorRoles:
+		# Process everything else as a command
+		await bot.process_commands(ctx)
 
 
 @bot.command(pass_context=True)
 async def commands(ctx, *args):
-    commandsEmbed = discord.Embed(title="Legionary Bot Help - By Bonf", color=0xffea00)
-    commandsEmbed.description = "Type !<command> to run a command!"
-    commandsEmbed.add_field(name="!members [update]",
-                            value='Lists all current members.\n[update] - Updates the previous members list',
-                            inline=False)
-    commandsEmbed.add_field(name="!recruit <name>", value='Recruits <name> to the clan', inline=False)
-    commandsEmbed.add_field(name="!promote <name>",
-                            value='Promotes <name> one rank higher (must already be recruited)', inline=False)
-    commandsEmbed.add_field(name="!remove <name>",
-                            value='Removes <name> from the server files', inline=False)
-    commandsEmbed.add_field(name="!kick <name>",
-                            value='Kicks <name> from the clan (discord and server files)', inline=False)
+	commandsEmbed = discord.Embed(title="Legionary Bot Help - By Bonf", color=0xffea00)
+	commandsEmbed.description = "Type !<command> to run a command!"
+	commandsEmbed.add_field(name="!members [update]",
+							value='Lists all current members.\n[update] - Updates the previous members list',
+							inline=False)
+	commandsEmbed.add_field(name="!recruit <name>", value='Recruits <name> to the clan', inline=False)
+	commandsEmbed.add_field(name="!promote <name>",
+							value='Promotes <name> one rank higher (must already be recruited)', inline=False)
+	commandsEmbed.add_field(name="!remove <name>",
+							value='Removes <name> from the server files', inline=False)
+	commandsEmbed.add_field(name="!kick <name>",
+							value='Kicks <name> from the clan (discord and server files)', inline=False)
 
-    await bot.send_message(ctx.message.channel, embed=commandsEmbed)
+	await bot.send_message(ctx.message.channel, embed=commandsEmbed)
 
 
 @bot.command(pass_context=True)
 async def recruit(ctx, user: discord.Member):
-    recruitName = user.display_name
-    recruitDate = datetime.datetime.today().strftime('%m/%d/%Y')
+	recruitName = user.display_name
+	recruitDate = datetime.datetime.today().strftime('%m/%d/%Y')
 
-    print("[Recruiting] Recruiting " + user.display_name)
+	print("[Recruiting] Recruiting " + user.display_name)
 
-    roleID = discord.utils.get(user.server.roles, name="Recruit")
-    await bot.add_roles(user, roleID)
+	roleID = discord.utils.get(user.server.roles, name="Recruit")
+	await bot.add_roles(user, roleID)
 
-    # Add their name to the members file
-    membersFile = open('Members', 'a')
-    membersFile.write("\n" + "Recruit" + "\t" + recruitName + "\t" + recruitDate)
-    membersFile.close()
+	# Add their name to the members file
+	membersFile = open('Members', 'a')
+	membersFile.write("\n" + "Recruit" + "\t" + recruitName + "\t" + recruitDate)
+	membersFile.close()
 
-    await bot.send_message(ctx.message.channel,
-                           "@everyone please welcome <@!%s> to the clan!" % user.id)
+	await bot.send_message(ctx.message.channel,
+						   "@everyone please welcome <@!%s> to the clan!" % user.id)
 
 
 @bot.command(pass_context=True)
 async def promote(ctx, user: discord.Member):
-    currentRole = user.top_role
-    if currentRole.name != 'Owner' and user.display_name != '@everyone':
-        newRoleNumber = legionRoles.index(currentRole.name) + 1
-        newRoleName = legionRoles[newRoleNumber]
+	currentRole = user.top_role
+	if currentRole.name != 'Owner' and user.display_name != '@everyone':
+		newRoleNumber = legionRoles.index(currentRole.name) + 1
+		newRoleName = legionRoles[newRoleNumber]
 
-        # Remove all roles and replace with the new one
-        newRoleID = discord.utils.get(ctx.message.server.roles, name=newRoleName)
-        moderatorRoleID = discord.utils.get(ctx.message.server.roles, name="Moderator")
-        globalModRoleID = discord.utils.get(ctx.message.server.roles, name="Global Moderator")
+		# Remove all roles and replace with the new one
+		newRoleID = discord.utils.get(ctx.message.server.roles, name=newRoleName)
+		moderatorRoleID = discord.utils.get(ctx.message.server.roles, name="Moderator")
+		globalModRoleID = discord.utils.get(ctx.message.server.roles, name="Global Moderator")
 
-        # Make sure we give the higher roles their moderator statuses
-        if newRoleName == 'Lieutenant' or newRoleName == 'Captain':
-            await bot.replace_roles(user, newRoleID, moderatorRoleID)
-        elif newRoleName == 'General':
-            await bot.replace_roles(user, newRoleID, globalModRoleID)
-        else:
-            await bot.replace_roles(user, newRoleID)
+		# Make sure we give the higher roles their moderator statuses
+		if newRoleName == 'Lieutenant' or newRoleName == 'Captain':
+			await bot.replace_roles(user, newRoleID, moderatorRoleID)
+		elif newRoleName == 'General':
+			await bot.replace_roles(user, newRoleID, globalModRoleID)
+		else:
+			await bot.replace_roles(user, newRoleID)
 
-        # Fetch all lines from the members file
-        membersFile = open('Members', "r")
-        lines = membersFile.readlines()
-        membersFile.close()
+		# Fetch all lines from the members file
+		membersFile = open('Members', "r")
+		lines = membersFile.readlines()
+		membersFile.close()
 
-        # Write every line except for the one being update by the new promotion
-        membersFile = open('Members', "w")
-        for line in lines:
-            if user.display_name not in line:
-                membersFile.write(line)
-        membersFile.close()
+		# Write every line except for the one being update by the new promotion
+		membersFile = open('Members', "w")
+		for line in lines:
+			if user.display_name not in line:
+				membersFile.write(line)
+		membersFile.close()
 
-        # Add the new entry to the members file
-        membersFile = open('Members', "a")
-        membersFile.write(
-            "\n" + newRoleName + "\t" + user.display_name + "\t" + datetime.datetime.today().strftime('%m/%d/%Y'))
-        membersFile.close()
+		# Add the new entry to the members file
+		membersFile = open('Members', "a")
+		membersFile.write(
+			"\n" + newRoleName + "\t" + user.display_name + "\t" + datetime.datetime.today().strftime('%m/%d/%Y'))
+		membersFile.close()
 
-        print("[Promotion] Promoted " + user.display_name + " to " + newRoleName)
+		print("[Promotion] Promoted " + user.display_name + " to " + newRoleName)
 
-        await bot.send_message(ctx.message.channel,
-                               "<@!%s> has been promoted to %s!" % (user.id, newRoleName))
+		await bot.send_message(ctx.message.channel,
+							   "<@!%s> has been promoted to %s!" % (user.id, newRoleName))
+
+
+async def removeFromFiles(memberName):
+	# Find their name in the members list
+	membersFile = open('Members', "r")
+	lines = membersFile.readlines()
+	membersFile.close()
+
+	# Write every line down except for theirs (exclude their name, thus removing it)
+	membersFile = open('Members', "w")
+	for line in lines:
+		if memberName not in line:
+			membersFile.write(line)
+	membersFile.close()
 
 
 @bot.command(pass_context=True)
-async def remove(ctx, *args):
-    removeName = args[0]
-
-    # Find their name in the members list
-    membersFile = open('Members', "r")
-    lines = membersFile.readlines()
-    membersFile.close()
-
-    # Write every line down except for theirs (exclude their name, thus removing it)
-    membersFile = open('Members', "w")
-    for line in lines:
-        if removeName not in line:
-            membersFile.write(line)
-    membersFile.close()
-
-    await bot.send_message(ctx.message.channel,
-                           "%s has been removed from the files!" % removeName)
+async def remove(ctx, user: discord.Member):
+	# Remove the player from the files
+	await removeFromFiles(user.display_name)
+	await bot.send_message(ctx.message.channel, "%s has been removed from the files!" % user.display_name)
 
 
 @bot.command(pass_context=True)
 async def kick(ctx, user: discord.Member):
-    # Find their name in the members list
-    membersFile = open('Members', "r")
-    lines = membersFile.readlines()
-    membersFile.close()
+	# Remove the player from the files
+	await removeFromFiles(user.display_name)
 
-    # Write every line down except for theirs (exclude their name, thus removing it)
-    membersFile = open('Members', "w")
-    for line in lines:
-        if user.display_name not in line:
-            membersFile.write(line)
-    membersFile.close()
-
-    # Kick the player from the clan
-    await bot.kick(user)
-
-    await bot.send_message(ctx.message.channel,
-                           "<@!%s> has been kicked from the clan!" % user)
+	# Kick the player from the clan
+	await bot.kick(user)
+	await bot.send_message(ctx.message.channel, "<@!%s> has been kicked from the clan!" % user)
 
 
 @bot.command(pass_context=True)
 async def members(ctx, *args):
-    # The best way I could think of doing this. I know I can make it better...
+	# The best way I could think of doing this. I know I can make it better...
 
-    # If they want to update, get rid of the older messages
-    if args == "update":
-        for index, message in enumerate(membersMessages):
-            await bot.delete_message(membersMessages[index])
+	# If they want to update, get rid of the older messages
+	if args == "update":
+		for index, message in enumerate(membersMessages):
+			await bot.delete_message(membersMessages[index])
 
-    for index, rank in enumerate(legionRoles):
-        membersFile = open('Members', 'r')
+	for index, rank in enumerate(legionRoles):
+		membersFile = open('Members', 'r')
 
-        # The title of the embed is the rank name, the color is the index of colors at the rank name
-        rankEmbed = discord.Embed(title=rank, color=legionColors[legionRoles.index(rank)])
-        rankDesc = ""
+		# The title of the embed is the rank name, the color is the index of colors at the rank name
+		rankEmbed = discord.Embed(title=rank, color=legionColors[legionRoles.index(rank)])
+		rankDesc = ""
 
-        membersLines = membersFile.readlines()
+		membersLines = membersFile.readlines()
 
-        for line in membersLines:
-            # Split the line by tabs to get the rank, name, and date
-            lineSplit = re.split(r'\t+', line)
+		for line in membersLines:
+			# Split the line by tabs to get the rank, name, and date
+			lineSplit = re.split(r'\t+', line)
 
-            # Strip (chomp in perl) the last line for the newline character
-            memberRank = lineSplit[0]
-            memberName = lineSplit[1]
-            memberDate = lineSplit[2].rstrip()
+			# Strip (chomp in perl) the last line for the newline character
+			memberRank = lineSplit[0]
+			memberName = lineSplit[1]
+			memberDate = lineSplit[2].rstrip()
 
-            # We go through each rank, rank by rank and make an embed out of it
-            if memberRank == rank:
-                rankDesc += (memberName + "\t" + memberDate + "\n")
-        membersFile.close()
+			# We go through each rank, rank by rank and make an embed out of it
+			if memberRank == rank:
+				rankDesc += (memberName + "\t" + memberDate + "\n")
+		membersFile.close()
 
-        rankEmbed.description = rankDesc
-        membersMessages[index] = await bot.send_message(ctx.message.channel, embed=rankEmbed)
+		rankEmbed.description = rankDesc
+		membersMessages[index] = await bot.send_message(ctx.message.channel, embed=rankEmbed)
 
 
 bot.run(botToken)
